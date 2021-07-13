4b9fa673b34727faf64f7b639a63efcb83244db5
Chris O'Brien <chrisobrien2000@outlook.com>
2021-07-13 13:09:16 -0500

    Added 'gamePlay' function to test higher order functions taking in lambda functions as arguments

--- src/main/kotlin/LambdaPractice.kt
+++ src/main/kotlin/LambdaPractice.kt
@@ -1,13 +1,21 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    val rollDice = { sides: Int ->
-        if (sides == 0) 0
-        else Random().nextInt(sides) + 1
-    }
     println("The Dice Roll for Lambda Version is: ${rollDice(50)}")
 
-    fun rollDice2(sides: Int): Int = if(sides == 0) 0 else Random().nextInt(sides) + 1
-
     println("The Dice Roll for Function Version is: ${rollDice2(50)}")
+
+    gamePlay(rollDice(20))
+    gamePlay(rollDice(20))
+}
+
+val rollDice = { sides: Int ->
+    if (sides == 0) 0
+    else Random().nextInt(sides) + 1
+}
+
+val rollDice2: (Int) -> Int = { sides -> if(sides == 0) 0 else Random().nextInt(sides) + 1 }
+
+fun gamePlay(diceRoll: Int){
+    println("The roll of the dice results in: $diceRoll")
 }
